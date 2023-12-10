40f4cf213c13e065c847b1c22d0eb352f903bfab
guoshijiang <guoshijiang2012@163.com>
2023-12-11 00:03:32 +0800

    project init

---
+++ script/Deployer.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+import "../src/test/mocks/EmptyContract.sol";
+
+import "forge-std/Script.sol";
+
+
+// forge script script/Deployer.s.sol:TreasureDeployer --rpc-url $RPC_URL  --private-key $PRIVATE_KEY --broadcast -vvvv
+contract PrivacyContractsDeployer is Script {
+    ProxyAdmin public savourTsProxyAdmin;
+    EmptyContract public emptyContract;
+    address[] pausers;
+
+    function run() external {
+        vm.startBroadcast();
+        // todo: write deploy script here
+        vm.stopBroadcast();
+    }
+}
