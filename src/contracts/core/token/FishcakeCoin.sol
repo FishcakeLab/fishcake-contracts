41875bf9a057fee81835d27faf5e97ec991e0775
imminer668 <imminer@163.com>
2024-03-30 20:28:12 +0800

    Record the number of tokens burned

--- src/contracts/core/token/FishcakeCoin.sol
+++ src/contracts/core/token/FishcakeCoin.sol
@@ -7,6 +7,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract FishcakeCoin is ERC20, Ownable {
     uint256 public constant MaxTotalSupply = 1_000_000_000 * 10 ** 18;
     bool MintingFinished = false;
+    uint256 public _burnedTokens; 
     address public MiningPool;
     address public DirectSalePool;
     address public InvestorSalePool;
@@ -14,43 +15,67 @@ contract FishcakeCoin is ERC20, Ownable {
     address public EarlyStageAirdropsPool;
     address public FoundationPool;
     address public RedemptionPool;
-    constructor() ERC20("Fishcake Coin", "FCC") Ownable(msg.sender) {
 
-    }
+    constructor() ERC20("Fishcake Coin", "FCC") Ownable(msg.sender) {}
 
     modifier onlyRedemptionPool() {
-        require(msg.sender == RedemptionPool,"Only RedemptionPool can call this function");
+        require(
+            msg.sender == RedemptionPool,
+            "Only RedemptionPool can call this function"
+        );
         _;
     }
 
-    function setPoolAddress(address _MiningPool, address _DirectSalePool, address _InvestorSalePool, address _NFTSalesRewardsPool, address _EarlyStageAirdropsPool, address _FoundationPool) public onlyOwner {
-
+    function setPoolAddress(
+        address _MiningPool,
+        address _DirectSalePool,
+        address _InvestorSalePool,
+        address _NFTSalesRewardsPool,
+        address _EarlyStageAirdropsPool,
+        address _FoundationPool
+    ) public onlyOwner {
         MiningPool = _MiningPool;
         DirectSalePool = _DirectSalePool;
         InvestorSalePool = _InvestorSalePool;
         NFTSalesRewardsPool = _NFTSalesRewardsPool;
         EarlyStageAirdropsPool = _EarlyStageAirdropsPool;
         FoundationPool = _FoundationPool;
-
     }
 
     function setRedemptionPool(address _RedemptionPool) public onlyOwner {
         RedemptionPool = _RedemptionPool;
     }
 
-
-    function PoolAllocation() public onlyOwner{
+    function PoolAllocation() public onlyOwner {
         require(MintingFinished == false, "Minting has been finished");
-        require(MiningPool != address(0), "Missing allocate MiningPool address");
-        require(DirectSalePool != address(0), "Missing allocate DirectSalePool address");
-        require(InvestorSalePool != address(0), "Missing allocate InvestorSalePool address");
-        require(NFTSalesRewardsPool != address(0), "Missing allocate NFTSalesRewardsPool address");
-        require(EarlyStageAirdropsPool != address(0), "Missing allocate EarlyStageAirdropsPool address");
-        require(FoundationPool != address(0), "Missing allocate FoundationPool address");
-        _mint(MiningPool, MaxTotalSupply * 3 / 10); // 30% of total supply
-        _mint(DirectSalePool, MaxTotalSupply * 2 / 10); // 20% of total supply
+        require(
+            MiningPool != address(0),
+            "Missing allocate MiningPool address"
+        );
+        require(
+            DirectSalePool != address(0),
+            "Missing allocate DirectSalePool address"
+        );
+        require(
+            InvestorSalePool != address(0),
+            "Missing allocate InvestorSalePool address"
+        );
+        require(
+            NFTSalesRewardsPool != address(0),
+            "Missing allocate NFTSalesRewardsPool address"
+        );
+        require(
+            EarlyStageAirdropsPool != address(0),
+            "Missing allocate EarlyStageAirdropsPool address"
+        );
+        require(
+            FoundationPool != address(0),
+            "Missing allocate FoundationPool address"
+        );
+        _mint(MiningPool, (MaxTotalSupply * 3) / 10); // 30% of total supply
+        _mint(DirectSalePool, (MaxTotalSupply * 2) / 10); // 20% of total supply
         _mint(InvestorSalePool, MaxTotalSupply / 10); // 10% of total supply
-        _mint(NFTSalesRewardsPool, MaxTotalSupply * 2 / 10); // 20% of total supply
+        _mint(NFTSalesRewardsPool, (MaxTotalSupply * 2) / 10); // 20% of total supply
         _mint(EarlyStageAirdropsPool, MaxTotalSupply / 10); // 10% of total supply
         _mint(FoundationPool, MaxTotalSupply / 10); // 10% of total supply
         MintingFinished = true;
@@ -58,12 +83,6 @@ contract FishcakeCoin is ERC20, Ownable {
 
     function burn(address user, uint256 _amount) public onlyRedemptionPool {
         _burn(user, _amount);
+        _burnedTokens+=_amount;
     }
-
-
-
-
-
-
-
 }
