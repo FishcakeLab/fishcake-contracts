48ab2fe185db156b88d46ba87a347533e58044b1
imminer668 <imminer@163.com>
2024-05-15 15:37:50 +0800

    update u address

--- src/contracts/core/sale/DirectSalePool.sol
+++ src/contracts/core/sale/DirectSalePool.sol
@@ -14,7 +14,7 @@ contract DirectSalePool is ReentrancyGuard {
     IERC20 public fishcakeCoin;
     RedemptionPool public redemptionPool;
     IERC20 public immutable USDT =
-        IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+        IERC20(0xc2132D05D31c914a87C6611C10748AEb04B58e8F);
 
     event BuyFishcakeCoinSuccess(
         address indexed buyer,
