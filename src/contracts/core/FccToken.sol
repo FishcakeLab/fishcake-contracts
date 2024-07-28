dc4e5caa4309487789bcbeb307d52ff71dbca7f2
imminer668 <imminer@163.com>
2024-07-28 23:07:39 +0800

    Add code comments

--- src/contracts/core/FccToken.sol
+++ src/contracts/core/FccToken.sol
@@ -5,6 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
+/**
+ * @title just for test 
+ * @dev FccToken is an ERC20 token with minting and burning functionality.
+ */
 contract FccToken is ERC20, Ownable, ERC20Permit {
     constructor(address initialOwner) ERC20("FccToken", "FCC") Ownable(initialOwner) ERC20Permit("FccToken") {}
 
--- src/contracts/core/UsdtToken.sol
+++ src/contracts/core/UsdtToken.sol
@@ -5,6 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
+/**
+ * @title just for test .
+ * @dev UsdtToken is an ERC20 token .
+ */
 contract UsdtToken is ERC20, Ownable, ERC20Permit {
     constructor(address initialOwner) ERC20("USDT", "USDT") Ownable(initialOwner) ERC20Permit("USDT") {}
 
