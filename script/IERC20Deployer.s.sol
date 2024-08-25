914080e1803889c507201afe98d7d6d7c7a5d648
xuegao <hubeixuegao@gmail.com>
2024-08-25 15:16:24 +0800

    fix test, test usdt decimal

--- script/IERC20Deployer.s.sol
+++ script/IERC20Deployer.s.sol
@@ -10,6 +10,10 @@ contract FccUsdtERC20 is ERC20 {
     constructor(string memory name, string memory symbol, uint256 initialSupply, address owner) ERC20(name, symbol) {
         _mint(owner, initialSupply);
     }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
 }
 
 contract IERC20Deployer is Script {
